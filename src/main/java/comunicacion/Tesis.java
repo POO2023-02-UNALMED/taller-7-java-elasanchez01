b2535fefa2b735b60e8a01ab423849861b5e60e3
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-10-10 05:24:23 -0500

    Update Tesis.java

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -1,5 +1,53 @@
 package comunicacion;
 
-public class Tesis {
+public class Tesis extends Escrito{
+	
+	private String idea;
+	private static String[] argumentos;
+	private String conclusion;
+	private String referencias;
+	private String interpretacion;
+	
+	public Tesis(String origen, String titulo, String autor, int paginas,String idea,String[] argumentos,String conclusion,String referencias,String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.idea = idea;
+		this.argumentos = argumentos;
+		this.conclusion = conclusion;
+		this.referencias = referencias;
+		this.interpretacion = interpretacion;
+	}
+	
+	public String getIdea() {
+		return idea;
+	}
+	public void setIdea(String idea) {
+		this.idea = idea;
+	}
+	public static String[] getArgumentos() {
+		return argumentos;
+	}
+	public static void setArgumentos(String[] argumentos) {
+		Tesis.argumentos = argumentos;
+	}
+	public String getConclusion() {
+		return conclusion;
+	}
+	public void setConclusion(String conclusion) {
+		this.conclusion = conclusion;
+	}
+	public String getReferencias() {
+		return referencias;
+	}
+	public void setReferencias(String referencias) {
+		this.referencias = referencias;
+	}
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	
 
 }
