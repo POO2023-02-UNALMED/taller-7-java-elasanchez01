da87276e5f4631b0061295a56d5f601054ec93dc
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-10-10 05:22:33 -0500

    Update Fabula.java

---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,31 @@
+package comunicacion;
+
+public class Fabula extends Escrito {
+	
+	private String ensenanza;
+	private String interpretacion;
+	
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.setEnsenanza(ensenanza);
+		this.setInterpretacion(interpretacion);
+	}
+
+	public String getEnsenanza() {
+		return ensenanza;
+	}
+
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza = ensenanza;
+	}
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	
+}
